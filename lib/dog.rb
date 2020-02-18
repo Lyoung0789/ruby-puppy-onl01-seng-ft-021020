7d8f41e1304895e1795929ed9932d127d68719fc
Lester Young <lyoung070789@gmail.com>
2020-02-18 23:08:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,7 +5,7 @@ class Dog
   
   def initialize (name)
     @name = name 
-    @@all << self
+    save
   end 
   
   def self.all 
@@ -18,4 +18,7 @@ class Dog
     end 
   end 
   
+  def save 
+    @@all << self
+  end 
 end 
